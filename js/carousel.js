0869cec791dfaeba0e9cf1a2a8337d0ae232f962
Jesaiah Prayor <JPrayor@dcas.nyc.gov>
2019-09-06 18:08:09 -0400

    Carousel left button not working

--- js/carousel.js
+++ js/carousel.js
@@ -11,16 +11,20 @@ const setSlidePosition = (slide,i)=>{
 
 slides.forEach(setSlidePosition);
 
+const moveSlide = (carousel, currentSlide, targetSlide)=>{
+    carousel.style.transform = `translateX(-${targetSlide.style.left})`;
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
 //Click Right Button move slide to the right
 rightButton.addEventListener('click', (e)=>{
     let currentSlide = carousel.querySelector('.current_slide');
-    let nextSlide = currentSlide.nextElementSibling.style.left;
-
-    carousel.style.transform= 'translateX(-' + nextSlide + ')';
+    let targetSlide = currentSlide.nextElementSibling;
+    moveSlide(carousel,currentSlide,targetSlide);
 })
 leftButton.addEventListener('click', (e)=>{
     let currentSlide = carousel.querySelector('.current_slide');
-    let nextSlide = currentSlide.previousElementSibling.style.right;
-
-    console.log(nextSlide);
+    let targetSlide = currentSlide.previousElementSibling;
+    moveSlide(carousel,currentSlide,targetSlide);
 })
