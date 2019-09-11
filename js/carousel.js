950d1986c7c79b68d51264ad254cd13371e41778
Jesaiah Prayor <JPrayor@dcas.nyc.gov>
2019-09-11 13:15:13 -0400

    added setTimeout to remove blackness

--- js/carousel.js
+++ js/carousel.js
@@ -18,12 +18,11 @@ const moveSlide = (carousel, currentSlide, targetSlide,name)=>{
 
     // classList.remove is not working
     currentSlide.classList.remove(currentSlideClass);
-    currentSlide.classList.remove('no_display');
     targetSlide.classList.remove('no_display');
     targetSlide.classList.add(currentSlideClass);
-    
-    console.log(carousel);
-
+    setTimeout(()=>{
+        currentSlide.classList.add('no_display');
+    }, 300)
 }
 
 //Click Right Button move slide to the right
